c7c8f5a529d7f42181978688cc3e962b309bf07c
Yuhanas Yulianto <yuhanas.yulianto@gmail.com>
2018-11-16 09:32:53 +0700

    fix if user input empty string

--- main.js
+++ main.js
@@ -18,25 +18,29 @@ userInput.on('keypress', function(e){
 // response when click addBtn or press enter after input text
 function addNewTask() {
   
-  list.append('<li>'); // append <li> as child of #list
-  $('#list li:last').append('<input>').append('<label>').append('<button>'); // append <input>, <label>, <button> as child of <li>
-  let checkBox = $('#list li input');
-  checkBox.attr('type', 'checkbox');
+  if (userInput.val() === "") {
+    alert('You must add something');
+  } else {
+    list.append('<li>'); // append <li> as child of #list
+    $('#list li:last').append('<input>').append('<label>').append('<button>'); // append <input>, <label>, <button> as child of <li>
+    let checkBox = $('#list li input');
+    checkBox.attr('type', 'checkbox');
 
-  let task = $('#list li:last label');
-  task.text(userInput.val());
+    let task = $('#list li:last label');
+    task.text(userInput.val());
 
-  let deleteBtn = $('#list li:last button');
-  deleteBtn.text('X').attr('class', 'close').click(destroy);
+    let deleteBtn = $('#list li:last button');
+    deleteBtn.text('X').attr('class', 'close').click(destroy);
 
-  userInput.val("");
-}
+    userInput.val("");
+  };
+};
 
 
 // response when close button clicked
 function destroy(e) {
   $(this).parent().remove();
-}
+};
 
 // // response if task is checked
 // $('li').on('click', function() {
