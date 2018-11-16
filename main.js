edee522f06787f00c0aa81a39918ffd808c3b654
Yuhanas Yulianto <yuhanas.yulianto@gmail.com>
2018-11-16 07:27:01 +0700

    yang penting push dulu

---
+++ main.js
@@ -0,0 +1,46 @@
+// $('#today-date').text(Date())
+
+let today = new Date();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+let addBtn = $('#add-task');
+let list = $('#list');
+let userInput = $('#input-task');
+
+//  add event click and enter
+addBtn.on('click', addNewTask);
+userInput.on('keypress', function(e){ 
+  if (e.keyCode == 13) {
+    addNewTask();
+  }
+});
+
+// response when click addBtn or press enter after input text
+function addNewTask() {
+  
+  list.append('<li>'); // append <li> as child of #list
+  $('#list li:last').append('<input>').append('<label>').append('<button>'); // append <input>, <label>, <button> as child of <li>
+  let checkBox = $('#list li input');
+  checkBox.attr('type', 'checkbox');
+
+  let task = $('#list li:last label');
+  task.text(userInput.val());
+
+  let deleteBtn = $('#list li:last button');
+  deleteBtn.text('X').attr('class', 'close').click(destroy);
+
+  userInput.val("");
+}
+
+
+// response when close button clicked
+function destroy(e) {
+  $(this).parent().remove();
+}
+
+// // response if task is checked
+// $('li').on('click', function() {
+//   $(this).attr('class', 'checked');
+// })
